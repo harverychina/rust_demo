10a7f24ae01b5084f466b1573b7a676c60a92d96
13929650@qq.com <13929650@qq.com>
2022-06-14 09:43:13 +0800

    rust基本数据类型

--- rustcrash/src/main.rs
+++ rustcrash/src/main.rs
@@ -1,8 +1,22 @@
-const X:i32 = 42;  // 定义常量，标识符采用大写形式
+// const X:i32 = 42;  // 定义常量，标识符采用大写形式
 
 fn main() {
     // let x = 1;  // 定义不可变量
     // let mut x = 1;
     // x = 42;  // 不可变量不能两次赋值，使用 mut关键字定义可变变量就可以重新赋值
-    print!("{}", X);
+    // print!("{}", X);
+
+    // let x = 2.0;   // f64
+    // let y: f32 = 3.0;   // f32
+
+    // let bool_true = true;
+    // let bool_false = false;
+
+    // let char_ascii = 'a';
+    // let char_emoji = '👊';
+
+    // print!("{}{}", char_ascii, char_emoji);
+
+    let array = [1, 2, 3, 4, 5];
+    print!("{}", array[0]);
 }
