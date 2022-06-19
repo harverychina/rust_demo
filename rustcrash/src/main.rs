050de73791018f30f9938560be994171fb7a727e
13929650@qq.com <13929650@qq.com>
2022-06-19 11:12:01 +0800

    更新

--- rustcrash/src/main.rs
+++ rustcrash/src/main.rs
@@ -47,7 +47,7 @@ fn main() {
         fn exploratioin(){
             assert_eq!(2 + 2, 4);
         }
-
+        
         #[test]
         fn another() {
             panic!("Make this test fail");
